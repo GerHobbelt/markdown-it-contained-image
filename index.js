292634af12b0f7dd02d66968396b3f050430df73
Andrea Gentili <andrea.gentili@bancaditalia.it>
2017-06-19 16:15:51 +0200

    Added support for data-interchange-like url structure.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = function(opts) {
     // this function will be called when something matches
     function(match) {
       var tag = opts.tag || 'div';
+      var interchangeAttribute = opts.tag || 'data-interchange';
 
       var caption = match[1] || '';
       var url = match[2] || '';
@@ -22,7 +23,26 @@ module.exports = function(opts) {
         caption = url;
       }
 
-      var imgTag = '<img src="' + url + '" alt="' + caption + '">';
+      var src = '';
+      if (url.indexOf(',') >= 0) {
+        // We have a data-interchange
+        var fields = url.split(',');
+        src = interchangeAttribute + '="';
+        fields.forEach(function(field, index) {
+          if (index > 0) {
+            src += ', ';
+          }
+          var items = field.split(':', 2);
+          if (items.length === 2) {
+            src += '[' + items[1] + ', (' + items[0] + ')]';
+          }
+        });
+        src += '"';
+      } else {
+        src = 'src="' + url + '"';
+      }
+
+      var imgTag = '<img ' + src + ' alt="' + caption + '">';
 
       var html = imgTag;
       for (var index = styles.length - 1; index >= 0; index--) {
